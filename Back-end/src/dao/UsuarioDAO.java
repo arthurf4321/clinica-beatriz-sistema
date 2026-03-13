5b6b4166528aaed56c75c493c9b3c329e5a6ff4c
Arthurf <arthurf43213@gmail.com>
2026-03-13 11:17:39 -0300

    feat(usuario): cria estrutura da classe UsuarioDAO

---
+++ Back-end/src/dao/UsuarioDAO.java
@@ -0,0 +1,31 @@
+package dao;
+import model.Usuario;
+import java.util.List;
+
+public class UsuarioDAO {
+
+    // Methods
+    public void criarUsuario(Usuario usuario) {
+
+    }
+
+    public  List<Usuario> listarUsuarios() {
+        return null;
+    }
+
+    public Usuario buscarUsuarioPorId(int id) {
+        return null;
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return null;
+    }
+
+    public void atualizarUsuario(Usuario usuario) {
+
+    }
+
+    public void deletarUsuario(int id) {
+
+    }
+}
