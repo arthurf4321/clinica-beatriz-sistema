b2c609c74f336cea4c51efd1c6a8e966c6a19e30
Parlinxdt <paulovitor9260@gmail.com>
2026-09-13 11:50:17 -0300

    feat: adiciona PacienteDAO e atualiza Paciente model

--- Back-end/src/model/Paciente.java
+++ Back-end/src/model/Paciente.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Paciente {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+
+    public Paciente(){}
+
+    public Paciente(int id, String nome, String cpf, LocalDate dataNascimento, String telefone, String email){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public LocalDate getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
